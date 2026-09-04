eae8910d7306851dc764134059ca58c062a109ac
FalakPatel213 <falakpatel213@gmail.com>
2026-09-04 17:21:43 +0530

    Updated Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import { CheckBox, Home, Info } from '@mui/icons-material';
+import { Home, Info } from '@mui/icons-material';
 
 function Header(props) {
     const { head = "Task Manager" } = props;
